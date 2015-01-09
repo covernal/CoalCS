4d56ba9400616cac545dd5d1996e1a6a7038053d
covernal <rock@rocks-Mac-mini.local>
2015-01-09 10:20:26 +0800

    init iOS project

---
+++ CoalCalCS.iOS/CoalCalCS.iOSViewController.designer.cs
@@ -0,0 +1,21 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace CoalCalCS.iOS
+{
+	[Register ("CoalCalCS_iOSViewController")]
+	partial class CoalCalCS_iOSViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
